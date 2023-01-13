1886c6524c42170d5605d840743be7aafe78c3af
Smit Jethwa <smitjethwa20@gmail.com>
2023-01-13 11:47:41 +0530

    Creds removed.

--- btre/settings.py
+++ btre/settings.py
@@ -89,9 +89,9 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': 'irctc',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST' : 'localhost'
         
     }
